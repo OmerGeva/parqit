798b7081c421e064277f5ca2f05418a3d7fc9023
Omer Geva <ogeva97@gmail.com>
2019-12-18 21:58:54 -0500

    added map to front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @parking_spots = ParkingSpot.geocoded
+
+    @markers = @parking_spots.map do |parking_spot|
+      {
+        lat: parking_spot.latitude,
+        lng: parking_spot.longitude
+      }
+    end
   end
 end
