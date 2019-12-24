a137565269ff19281a73ff13b4b376c212f16c79
Omer Geva <ogeva97@gmail.com>
2019-12-23 19:32:49 -0500

    commented out algolia... go back and revise

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,8 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import "../plugins/flatpickr"
 
 import { initMapbox } from '../plugins/init_mapbox';
+// import { initAddressAutocomplete } from '../plugins/init_autocomplete'
 
 
 initMapbox();
+// initAddressAutocomplete();
