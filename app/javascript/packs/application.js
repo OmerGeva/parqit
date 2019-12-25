e975de7a400e5afc3e548c9be59abb63b17e92a3
Omer Geva <ogeva97@gmail.com>
2019-12-24 19:23:13 -0500

    added photo uploaded js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,12 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 import "../plugins/flatpickr"
 
+import { previewImageOnFileSelect } from "../components/photo_preview"
+
+
 import { initMapbox } from '../plugins/init_mapbox';
 // import { initAddressAutocomplete } from '../plugins/init_autocomplete'
 
-
+previewImageOnFileSelect();
 initMapbox();
 // initAddressAutocomplete();
