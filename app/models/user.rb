1ad2b2f60955274bf3efde6bba8716973c8f5cab
Omer Geva <ogeva97@gmail.com>
2019-12-27 23:35:24 -0500

    image hosting for profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,26 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  before_create :set_picture
+  after_create :make_account, :capitalize_name
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 
   has_many :bookings
   has_many :parking_spots
   has_one :account
 
-  after_create :make_account
-
+  mount_uploader :photo, PhotoUploader
   private
+
+  def set_picture
+    self.remote_photo_url = 'https://i.kym-cdn.com/photos/images/newsfeed/001/452/271/0f9'
+  end
+
+  def capitalize_name
+    self.update(full_name: (self.full_name.split(' ').map { |name| name.capitalize }).join(' '))
+  end
 
   def make_account
     Account.create(user: self)
