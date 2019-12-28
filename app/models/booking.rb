6ff05b5f39b65073fa6eabe62e736b3ab3e2d5ec
Omer Geva <ogeva97@gmail.com>
2019-12-28 18:27:45 -0500

    added validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :parking_spot
+
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :date, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,7 @@ class Review < ApplicationRecord
   belongs_to :parking_spot
 
   validates :rating, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
+
 end
