ea4195db0ac61c2a4eaaa4e40e4bc4aded6730ee
Omer Geva <ogeva97@gmail.com>
2019-12-30 17:48:02 -0500

    navbar moves not shrinks

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,10 +3,10 @@ const toggleNavbar = () => {
   const closeNavBtn = document.getElementById('close-nav-btn')
 
   openNavBtn.addEventListener('click', () => {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.left = "0";
   })
   closeNavBtn.addEventListener('click', () => {
-    document.getElementById("mySidenav").style.width = "0px";
+    document.getElementById("mySidenav").style.left = "-250px";
   })
 }
 
